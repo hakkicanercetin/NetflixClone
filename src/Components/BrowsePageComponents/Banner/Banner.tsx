37fd9a0cd77fbd53fcdd15d41c6de2d953deea4e
Hakkıcan ERÇETİN <hakkicanercetin@gmail.com>
2024-03-18 05:12:19 +0300

    Update

--- src/Components/BrowsePageComponents/Banner/Banner.tsx
+++ src/Components/BrowsePageComponents/Banner/Banner.tsx
@@ -5,7 +5,7 @@ import { useEffect, useState } from "react";
 import { Movie } from "../../../types/Movie";
 import { Trailer } from "../../../types/Trailer";
 const getTrailer = async (movie:Movie) => {
-  const apiKey = process.env.TMDB_API_KEY
+  const apiKey = process.env.TMDB_API_KEY;
   console.log(apiKey)
   const response = await fetch("https://api.themoviedb.org/3/movie/"+movie.id+"/videos?language=en-US&api_key="+apiKey)
   const data:Trailer = await response.json()
