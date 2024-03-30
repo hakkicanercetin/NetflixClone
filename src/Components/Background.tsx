a2b6475549c3bf2b4726e658f40c9be2ba986398
Hakkıcan ERÇETİN <hakkicanercetin@gmail.com>
2024-03-30 06:27:19 +0300

    Update

--- src/Components/Background.tsx
+++
@@ -1,9 +0,0 @@
-const Background = () => {
-  return (
-    <div className='smm:bg-cover smm:block smm:h-full smm:min-h-[100vh] smm:overflow-hidden smm:absolute smm:w-full z-[-1] opacity-50 hidden'>
-        <img className="smm:min-w-full smm:min-h-full max-w-none" src="https://assets.nflxext.com/ffe/siteui/vlv3/93da5c27-be66-427c-8b72-5cb39d275279/c06f9417-bf37-4b6f-a997-0e9ce16c2b29/TR-en-20240226-popsignuptwoweeks-perspective_alpha_website_large.jpg" alt="" />
-      </div>
-  )
-}
-
-export default Background
